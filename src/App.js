45138f7815d95317685f020473aba6bc02f289c3
Umirunjon <umirun009@gmail.com>
2025-11-10 17:20:20 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"; // Stil faylimizni chaqiryapmiz
 
 function App() {
   return (
- <p>saytimga Hush kelibsiz </p>
+    <div className="container">
+      {/* Birinchi rasm */}
+      <div className="image-wrapper">
+        <img
+          src="https://picsum.photos/id/10/200/200" // Rasmning linki
+          alt="Aylanuvchi rasm 1"
+          className="rotating-image"
+        />
+      </div>
+
+      {/* Ikkinchi rasm */}
+      <div className="image-wrapper">
+        <img
+          src="https://picsum.photos/id/20/200/200" // Ikkinchi rasmning linki
+          alt="Aylanuvchi rasm 2"
+          className="rotating-image"
+        />
+      </div>
+    </div>
   );
 }
 
