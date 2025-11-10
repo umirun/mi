092730d3ef7da5828467d81da5c163f657ded857
botirov0102 <asrorbotirov127@gmail.com>
2025-11-10 17:43:42 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       {/* Birinchi rasm */}
       <div className="image-wrapper">
         <img
-          src="https://picsum.photos/id/10/200/200" // Rasmning linki
+          src="https://www.google.com/imgres?q=menicraft&imgurl=https%3A%2F%2Fartline.ua%2Fstorage%2Fimages%2Feditor%2Feditor_1609241070375106_0.jpg&imgrefurl=https%3A%2F%2Fartline.ua%2Fnews%2Figrovoy-pk-minecraft&docid=m-uWZiW0GZswoM&tbnid=KwdYdV_F6MfFcM&vet=12ahUKEwiGm-H9zeeQAxUTPhAIHUVkIfAQM3oECBwQAA..i&w=750&h=500&hcb=2&ved=2ahUKEwiGm-H9zeeQAxUTPhAIHUVkIfAQM3oECBwQAA" // Rasmning linki
           alt="Aylanuvchi rasm 1"
           className="rotating-image"
         />
